6ea5856494193f3383d7d1ec89d4a3d091eaaffe
Kelvyn <kelvyn.valle@gmail.com>
2021-10-14 15:19:36 -0300

    Adição de controle de medidas de dissimilaridade e técnicas de agrupamento (vizinho mais próximo e mais distante)

---
+++ 01 - Clustering, tratamento de dados e dendrogramas/Graficos/Diagrama.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ClusterCSV.Graficos
+{
+
+    //Enumeração que representa os tipos de ações que se pode realizar em desenhos virtuals
+    enum acoes
+    {
+        linha,
+        spline,
+        texto
+    }
+
+    //Classe destinada ao desenho virtual de diagramas e suas conversões em bitmap
+    class Diagrama
+    {
+        private int _largura;
+        private int _altura;
+        private int _margemX;
+        private int _margemY;
+        List<Tuple<acoes, object>> desenho = new List<Tuple<acoes, object>>();
+
+        public int largura
+        {
+            get { return _largura; }
+            set { _largura = value; }
+        }
+        public int altura
+        {
+            get { return _altura; }
+            set { _altura = value; }
+        }
+        public int margemX
+        {
+            get { return _margemX; }
+            set { _margemX = value; }
+        }
+        public int margemY
+        {
+            get { return _margemY; }
+            set { _margemY = value; }
+        }
+
+        //Método destinado à desenhar linha virtual entre dois pontos
+        public void desenharLinha(PointF inicio, PointF fim)
+        {
+            desenho.Add(Tuple.Create(acoes.linha, (object)new PointF[] { inicio, fim }));
+        }
+
+        //Método destinado à desenhar texto virtual em determinada posição
+        public void desenharTexto(PointF posicao, string valor)
+        {
+            desenho.Add(Tuple.Create(acoes.texto, (object)(new object[] { (object)posicao, (object)valor })));
+        }
+
+        //Método destinado à desenhar spline a partir de vetor de pontos
+        public void desenharSpline(PointF[] pontos)
+        {
+            desenho.Add(Tuple.Create(acoes.linha, (object)pontos));
+
+        }
+
+        //Método privado para converter pontos genéricos no valor real do desenho, utilizando sua largura, altura e margens
+        //Desenhos são efetivamente realizados a partir da conversão de seus pontos virtuais nos valores reais
+        private PointF converterPonto(PointF ponto)
+        {
+            return new PointF((ponto.X * largura / larguraMaxima()) + margemX * 0.5f, (ponto.Y * altura / alturaMaxima()) + margemY * 0.5f);
+        }
+
+        //Retorna a altura virtual máxima que o desenho pode ter
+        private float alturaMaxima()
+        {
+            float max = 0;
+            for (int i = 0; i < desenho.Count; i++)
+            {
+                if (desenho[i].Item1 == acoes.linha)
+                {
+                    PointF[] pontos = (PointF[])desenho[i].Item2;
+                    float atual = pontos[0].Y > pontos[1].Y ? pontos[0].Y : pontos[1].Y;
+                    max = max > atual ? max : atual;
+                }
+                else if (desenho[i].Item1 == acoes.texto)
+                {
+                    PointF pontos = (PointF)((object[])desenho[i].Item2)[0];
+                    max = max > pontos.Y ? max : pontos.Y;
+                }
+                else if (desenho[i].Item1 == acoes.spline)
+                {
+                    //não há splines ainda no projeto
+                }
+            }
+            return max;
+        }
+
+        //Retorna a largura virtual máxima que o desenho pode ter
+        private float larguraMaxima()
+        {
+            float max = 0;
+            for (int i = 0; i < desenho.Count; i++)
+            {
+                if (desenho[i].Item1 == acoes.linha)
+                {
+                    PointF[] pontos = (PointF[])desenho[i].Item2;
+                    float atual = pontos[0].X > pontos[1].X ? pontos[0].X : pontos[1].X;
+                    max = max > atual ? max : atual;
+                }
+                else if (desenho[i].Item1 == acoes.texto)
+                {
+                    PointF pontos = (PointF)((object[])desenho[i].Item2)[0];
+                    max = max > pontos.X ? max : pontos.X;
+                }
+                else if (desenho[i].Item1 == acoes.spline)
+                {
+                    //não há splines ainda no projeto
+                }
+            }
+            return max;
+        }
+
+        //Converte o desenho virtual em bitmap
+        public Bitmap desenharDiagrama()
+        {
+            Bitmap diagrama = new Bitmap(_largura + margemX, _altura + margemY);
+            Graphics g = Graphics.FromImage(diagrama);
+            g.Clear(Color.White);
+            foreach (Tuple<acoes, object> acao in desenho)
+            {
+
+                switch (acao.Item1)
+                {
+                    case acoes.linha:
+                        PointF[] pontos_linha = (PointF[])acao.Item2;
+                        g.DrawLine(new Pen(new SolidBrush(Color.Black)), converterPonto(pontos_linha[0]), converterPonto(pontos_linha[1]));
+                        break;
+                    case acoes.spline:
+                        PointF[] pontos_spline = (PointF[])acao.Item2;
+                        Array.ForEach(pontos_spline, ponto => converterPonto(ponto));
+                        g.DrawCurve(new Pen(new SolidBrush(Color.Black)), pontos_spline);
+                        break;
+                }
+            }
+            //como o desenho é realizado "ao contrário" rotaciona o diagrama 180° para deixá-lo na posição correta
+            //Isso deve ser feito antes de se escrever o texto para evitar que eles fiquem de cabeça para baixo
+            diagrama.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            var textos = desenho.FindAll(x => x.Item1 == acoes.texto).ToArray();
+            for (int i = 0; i < textos.Length; i++)
+            {
+                Object[] parametros = (Object[])textos[i].Item2;
+                PointF posicao = converterPonto((PointF)parametros[0]);
+                string texto = (string)parametros[1];
+                posicao = new PointF((_largura + _margemX) - (posicao.X + (texto.Length / 2) * 10) + 1.0f, (_altura + _margemY) - (posicao.Y + 5) + 1.0f);
+                g.DrawString(texto, new Font(new FontFamily("arial"), 10), new SolidBrush(Color.Black), posicao);
+            }
+            g.Dispose();
+            return diagrama;
+        }
+    }
+}
---
+++ 01 - Clustering, tratamento de dados e dendrogramas/Program.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using ClusterCSV.Graficos;
+using ClusterCSV.Dados;
+using ClusterCSV.Enumerações;
+namespace ClusterCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura = 300;
+            int largura = 300;
+            int margemX = 50;
+            int margemY = 50;
+            metodo metodo = metodo.vizinhoMaisProximo;
+            similaridadeDissimilaridade similaridadeDissimilaridade = similaridadeDissimilaridade.distancia;
+            Dataset dataset = new Dataset();
+            while (true)
+            {
+                Console.WriteLine("Digite um comando.\nPara ver a lista de comandos, digite \"ajuda\".");
+                switch (Console.ReadLine().ToLower())
+                {
+                    case "ajuda":
+                        //Exibe todos os comandos
+                        Console.WriteLine("ajuda - Exibe a lista de comandos disponíveis\n" +
+                            "agrupar - Agrupa amostras em clusters\n" +
+                            "altura - Define altura de gráficos e imagens.\n" +
+                            "dendrograma - Salva dendrograma em caminho especificado\n" +
+                            "desvio - Exibe desvio padrão de dataset carregado\n" +
+                            "dissimilaridade - Define a medida de dissimilaridade adotada\n" +
+                            "largura - Define largura de gráficos e imagens.\n" +
+                            "ler - A partir de um caminho, lê dataset em CSV\n" +
+                            "limpar - Limpa a tela.\n" +
+                            "média - Exibe média de dataset carregado.\n" +
+                            "margemX - Define margem horizontal.\n" +
+                            "margemY - Define margem vertical.\n" +
+                            "método - Define o método de agrupamento.\n" +
+                            "salvar - Salva dataset normalizado.\n" +
+                            "");
+                        break;
+                    case "agrupar":
+                        //Chama funções de agrupamento
+                        try
+                        {
+                            dataset.agrupar(metodo, similaridadeDissimilaridade);
+                            Console.WriteLine("Agrupamento definido!");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Erro ao tentar agrupar o dataset. Verifique os valores.");
+                        }
+                        break;
+                    case "altura":
+                        //Permite que o usuário modifique a altura de saída dos diagramas
+                        //Altura em pixels
+                        Console.WriteLine("Digite a altura de saída de gráficos e imagens:");
+                        string _altura = Console.ReadLine();
+                        if (!int.TryParse(_altura, out _))
+                        {
+                            Console.WriteLine("Valor inválido.");
+                        }
+                        else
+                        {
+                            altura = int.Parse(_altura);
+                            Console.WriteLine("Altura definida.");
+                        }
+                        break;
+                    case "dendrograma":
+                        //Desenha e salva dendrograma
+                        //Salvo em bmp
+                        try
+                        {
+                            Console.WriteLine("Escreva o caminho para salvar o dendrogama: (exemplo: c:\\pasta\\arquivo.bmp)");
+                            string caminho = Console.ReadLine();
+                            if (!caminho.Contains("bmp"))
+                            {
+                                Console.WriteLine("Caminho inválido.");
+                                break;
+                            }
+                            Dendrograma dendrograma = new Dendrograma();
+                            dendrograma.altura = altura;
+                            dendrograma.largura = largura;
+                            dendrograma.margemX = margemX;
+                            dendrograma.margemY = margemY;
+                            dendrograma.amostras = dataset.obterNomes();
+                            var diagrama = dendrograma.desenharPartindoDeRelacoes(dataset.mapaRelacoes, dataset.tamanho);
+                            diagrama.Save(caminho);
+                            Console.WriteLine("Arquivo salvo.");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Erro. Verifique se o caminho é válido.");
+                        }
+                        break;
+                    case "desvio":
+                        //Exibe desvio padrão amostral do dataset
+                        if (dataset is null)
+                        {
+                            Console.WriteLine("Andes de verificar o desvio padrão, carregue um dataset.");
+                            break;
+                        }
+                        Console.WriteLine("Desvio:\n");
+                        for (int desvio = 0; desvio < dataset.desvioPadrao.Length; desvio++)
+                        {
+                            Console.WriteLine(dataset.titulos[desvio] + ": " + dataset.desvioPadrao[desvio].ToString());
+                        }
+                        break;
+                    case "dissimilaridade":
+                        //define qual será a medida de dissimilaridade escolhida para avaliar as amostras
+                        //Quanto maior for a medida de dissimilaridade, menor será a semelhança entre os elementos
+                        Console.WriteLine("Escolha uma medida de dissimilaridade:\n1 - Distância euclidiana\n2 - Distância euclidiana ao quadrado");
+                        string medida_escolhida = Console.ReadLine();
+                        switch (medida_escolhida)
+                        {
+                            case "1":
+                                similaridadeDissimilaridade = similaridadeDissimilaridade.distancia;
+                                Console.WriteLine("Medida de dissimilaridade definida com sucesso!");
+                                break;
+                            case "2":
+                                similaridadeDissimilaridade = similaridadeDissimilaridade.distanciaQuadrado;
+                                Console.WriteLine("Medida de dissimilaridade definida com sucesso!");
+                                break;
+                            default:
+                                Console.WriteLine("Erro. Digite uma das opções numéricas dadas.");
+                                break;
+                        }
+                        break;
+                    case "largura":
+                        //Permite que o usuário modifique a largura de saída do diagrama
+                        //Largura em pixels
+                        Console.WriteLine("Digite a largura de saída de gráficos e imagens:");
+                        string _largura = Console.ReadLine();
+                        if (!int.TryParse(_largura, out _))
+                        {
+                            Console.WriteLine("Valor inválido.");
+                        }
+                        else
+                        {
+                            largura = int.Parse(_largura);
+                            Console.WriteLine("Largura definida.");
+                        }
+                        break;
+                    case "ler":
+                        //Permite que o sistema leia arquivo fornecido por usuário
+                        //Arquivo em CSV
+                        Console.WriteLine("Digite o caminho do arquivo. Exemplo: C:\\Arquivo.csv");
+                        var arquivo = Console.ReadLine();
+                        if (!arquivo.Contains(".csv"))
+                        {
+                            Console.WriteLine("Arquivo inválido. Utilize uma tabela CSV.");
+                            break;
+                        }
+                        try
+                        {
+                            dataset.lerArquivo(arquivo);
+                            Console.WriteLine("Arquivo carregado com sucesso!");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Erro na leitura da tabela. Verifique a consistência dos dados.\n" +
+                                "Certifique-se de que a tabela está no padrão:\n" +
+                                "Primeira coluna: identificadores de amostra (texto)\n" +
+                                "Primeira linha: identificadores de coluna (texto)\n" +
+                                "Demais celulas: valores numéricos.");
+                        }
+                        break;
+                    case "limpar":
+                        //Permite que o usuário limpe o console
+                        Console.Clear();
+                        break;
+                    case "média":
+                        //Exibe médias do dataset
+                        if (dataset is null)
+                        {
+                            Console.WriteLine("Andes de verificar a média, carregue um dataset.");
+                            break;
+                        }
+                        Console.WriteLine("Média:\n");
+                        for (int media = 0; media < dataset.media.Length; media++)
+                        {
+                            Console.WriteLine(dataset.titulos[media] + ": " + dataset.media[media].ToString());
+                        }
+                        break;
+                    case "margemx":
+                        //Permite que o usuário modifique a margem horizontal do diagrama
+                        //Valor em pixels
+                        Console.WriteLine("Digite a margem horizontal de saída de gráficos e imagens:");
+                        string _margemX = Console.ReadLine();
+                        if (!int.TryParse(_margemX, out _))
+                        {
+                            Console.WriteLine("Valor inválido.");
+                        }
+                        else
+                        {
+                            margemX = int.Parse(_margemX);
+                            Console.WriteLine("Margem horizontal definida.");
+                        }
+                        break;
+                    case "margemy":
+                        //Permite que o usuário modifique a marvem vertical do diagrama
+                        //Valor em pixels
+                        Console.WriteLine("Digite a margem vertical de saída de gráficos e imagens:");
+                        string _margemY = Console.ReadLine();
+                        if (!int.TryParse(_margemY, out _))
+                        {
+                            Console.WriteLine("Valor inválido.");
+                        }
+                        else
+                        {
+                            margemY = int.Parse(_margemY);
+                            Console.WriteLine("Margem vertical definida.");
+                        }
+                        break;
+                    case "método":
+                        //Permite que o usuário selecione o método de agrupamento
+                        Console.WriteLine("Defina o método de agrupamento. \nDigite 1 para vizinho mais próximo\nDigite 2 para vizinho mais distante.");
+                        string metodo_escolhido = Console.ReadLine();
+                        switch (metodo_escolhido)
+                        {
+                            case "1":
+                                metodo = metodo.vizinhoMaisProximo;
+                                Console.WriteLine("Método definido com sucesso!");
+                                break;
+                            case "2":
+                                metodo = metodo.vizinhoMaisDistante;
+                                Console.WriteLine("Método definido com sucesso!");
+                                break;
+                            default:
+                                Console.WriteLine("Erro. Digite uma das opções numéricas dadas.");
+                                break;
+                        }
+                        break;
+                    case "salvar":
+                        //Permite que o usuário salve nova tabela CSV
+                        //Dataset normalizado e com atributos modificados
+                        Console.WriteLine("Digite o caminho do arquivo. Exemplo: C:\\Arquivo.csv");
+                        var arquivoCSV = Console.ReadLine();
+                        if (!arquivoCSV.Contains(".csv"))
+                        {
+                            Console.WriteLine("Arquivo inválido. Utilize uma tabela CSV.");
+                            break;
+                        }
+                        try
+                        {
+                            dataset.salvar(arquivoCSV);
+                            Console.WriteLine("Arquivo salvo com sucesso!");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Erro ao salvar tabela. Verifique a consistência dos dados.\n" +
+                                "Certifique-se de que o caminho está correto.");
+                        }
+                        break;
+                }
+            }
+
+        }
+    }
+}
